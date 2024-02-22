e916952dd505287eae33cd16ce7df9f4e8510ad7
enrico-amerio <amerio.enrico95@gmail.com>
2024-02-22 16:12:02 +0100

    [add] graphic design to page

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
-for(let i = 1; i < 100; i++){
+const container = document.querySelector('.container');
+
+for(let i = 1; i <= 100; i++){
   if (i % 3 === 0 && i % 5 === 0){
     console.log('FizzBuzz')
+    container.innerHTML += `
+     <div class="box fizzbuzz"> FizzBuzz </div>
+     `
   } else if (i % 3 === 0){
     console.log('Fizz')
+    container.innerHTML += `
+     <div class="box fizz"> Fizz </div>
+     `
   } else if(i % 5 === 0){
     console.log('Buzz')
+    container.innerHTML += `
+     <div class="box buzz"> buzz </div>
+     `
   }else{
     console.log(i)
+    container.innerHTML += `
+     <div class="box"> ${i} </div>
+     `
   }
 }
